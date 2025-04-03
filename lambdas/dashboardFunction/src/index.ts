33088f56701979db6d30e2af7ce7a7ca31178a5f
hirohata <hirohatatro@gmail.com>
2025-04-03 14:38:17 -0700

    feat: set up Biome

--- lambdas/dashboardFunction/src/index.ts
+++ lambdas/dashboardFunction/src/index.ts
@@ -1,21 +1,21 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/aws-lambda'
+import { Hono } from "hono";
+import { handle } from "hono/aws-lambda";
 import { serve } from "@hono/node-server";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
 
 // This condition checks if the code is running in an AWS Lambda environment by checking the function name
 // If it is, it uses the AWS Lambda handler
 // If not, it starts a local server
 if (process.env.AWS_LAMBDA_FUNCTION_NAME) {
-  // this is lambda function handler
-  exports.handler = handle(app)
+	// this is lambda function handler
+	exports.handler = handle(app);
 } else {
-  // this is local server
-  console.log("Server is running on http://localhost:3000")
-  serve(app)
+	// this is local server
+	console.log("Server is running on http://localhost:3000");
+	serve(app);
 }
