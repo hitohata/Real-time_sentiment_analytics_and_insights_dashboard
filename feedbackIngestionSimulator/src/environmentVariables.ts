78c68a5a118a2b1b43a5986263d5086a5ad7c789
hitohata <hirohatatro@gmail.com>
2025-04-03 23:10:51 -0700

    feat: added a feedback ingestion simulator

---
+++ feedbackIngestionSimulator/src/environmentVariables.ts
@@ -0,0 +1,18 @@
+/**
+ * This function returns the URL of the feedback endpoint.
+ * If the DEBUG environment variable is set to true, it returns the localhost URL.
+ * @returns The URL of the feedback endpoint.
+ */
+const url = () => {
+    // If the DEBUG environment variable is set to true, use localhost
+    if (process.env.DEBUG === 'true') {
+        return 'http://localhost:3000/feedbacks'
+    }
+
+    // this is for the production environment
+    const hostName =  process.env.API_GATEWAY_HOSTNAME;
+    if (!hostName) throw new Error('The environment variable, API_GATEWAY_HOSTNAME, is not set');
+    return `${hostName}/feedbacks`;
+}
+
+export const FEEDBACK_API_URL = url();
