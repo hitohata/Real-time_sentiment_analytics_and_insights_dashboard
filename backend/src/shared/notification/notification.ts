e6ff9cc0f5789989a16b8893e9a3ad2dacdb552c
hitohata <hirohatatro@gmail.com>
2025-04-05 22:00:42 -0700

    chore: format

--- backend/src/shared/notification/notification.ts
+++ backend/src/shared/notification/notification.ts
@@ -1,9 +1,9 @@
 export interface INotification {
-    /**
-     * send a notification to the users
-     * @param notificationMessage
-     */
-    sendNotification(notificationMessage: string): Promise<void>;
+	/**
+	 * send a notification to the users
+	 * @param notificationMessage
+	 */
+	sendNotification(notificationMessage: string): Promise<void>;
 }
 
 /**
@@ -13,25 +13,23 @@ export interface INotification {
  * Once the `sendNotification` method is  called, it will call all the services in the list
  */
 export class NotificationImpl implements INotification {
-    private readonly notificationServices: INotification[];
+	private readonly notificationServices: INotification[];
 
-    constructor(notificationServices: INotification[]) {
-        this.notificationServices = notificationServices;
-    }
+	constructor(notificationServices: INotification[]) {
+		this.notificationServices = notificationServices;
+	}
 
-    async sendNotification(notificationMessage: string): Promise<void> {
-        await Promise.all(
-            this.notificationServices.map((service) =>
-                service.sendNotification(notificationMessage),
-            ),
-        );
-    }
-
+	async sendNotification(notificationMessage: string): Promise<void> {
+		await Promise.all(
+			this.notificationServices.map((service) =>
+				service.sendNotification(notificationMessage),
+			),
+		);
+	}
 }
 
 export class MockNotification implements INotification {
-
-    async sendNotification(notificationMessage: string): Promise<void> {
-        return Promise.resolve();
-    }
+	async sendNotification(notificationMessage: string): Promise<void> {
+		return Promise.resolve();
+	}
 }
